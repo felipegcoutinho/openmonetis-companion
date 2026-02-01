c7e52586c3f37b84c22b67047ffbfcfa31430ace
Felipe Coutinho <felipi@live.com>
2026-02-01 13:58:44 +0000

    Corrigir duplicação de buildTypes no build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,18 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(project.findProperty("android.injected.signing.store.file") ?: "keystore.jks")
+            storePassword = project.findProperty("android.injected.signing.store.password") as String?
+            keyAlias = project.findProperty("android.injected.signing.key.alias") as String?
+            keyPassword = project.findProperty("android.injected.signing.key.password") as String?
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
@@ -46,26 +56,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-     signingConfigs {
-        create("release") {
-            storeFile = file(project.findProperty("android.injected.signing.store.file") ?: "keystore.jks")
-            storePassword = project.findProperty("android.injected.signing.store.password") as String?
-            keyAlias = project.findProperty("android.injected.signing.key.alias") as String?
-            keyPassword = project.findProperty("android.injected.signing.key.password") as String?
-        }
-    }
-    
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
 }
 
